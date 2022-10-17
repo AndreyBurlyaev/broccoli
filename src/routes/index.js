815541fe92a9b531dcd515e086da1b470118b812
Julia <tarasova.bmstu@gmail.com>
2022-10-17 10:20:56 +0300

    render and locals added

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,12 @@
 import { Router } from 'express';
-import { renderToString } from 'react-dom/server';
-import React from 'react';
 import { Entry } from '../db/models';
-import Layout from '../components/Layout';
 
 const router = Router();
 
 router.get('/', async (req, res) => {
   const entries = await Entry.findAll({ order: [['id', 'DESC']] });
-  const initState = { path: req.originalUrl, entries };
-  const layout = React.createElement(Layout, { initState });
-  const html = renderToString(layout);
-  res.write('<!DOCTYPE html>');
-  res.end(html);
+  const initState = { entries };
+  res.render('Layout', initState);
 });
 
 export default router;
