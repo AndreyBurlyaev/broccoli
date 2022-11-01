6c3ca1fb43bbda58924efbce65b0f7f8a96dc171
Adam Balkoev <adam22185515@gmail.com>
2022-11-01 12:13:45 +0300

    Remove useEffect and Link

--- src/components/EntryItem.jsx
+++ src/components/EntryItem.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 export default function EntryItem({ entry }) {
   return (
     <li className="entry-item pad-b-4">
-      <Link to={`/entries/${entry.id}`} className="entry-title font-2 pad-b-1-4 c-white">{entry.title}</Link>
+      <a href={`/entries/${entry.id}`} className="entry-title font-2 pad-b-1-4 c-white">{entry.title}</a>
       <p className="entry-stub">{entry.body}</p>
     </li>
   );
--- src/routes/entries.js
+++ src/routes/entries.js
@@ -3,15 +3,32 @@ import { Entry } from '../db/models';
 
 const router = Router();
 
-router.get('/new', (req, res) => {
-  const initState = { };
-  res.render('Layout', initState);
-});
+router.route('/new')
+  .get((req, res) => {
+    const initState = {};
+    res.render('Layout', initState);
+  })
+  .post(async (req, res) => {
+    await Entry.create(req.body);
+    res.redirect('/');
+  });
 
-router.get('/:id/edit', async (req, res) => {
-  const entry = await Entry.findOne({ where: { id: req.params.id } });
-  const initState = { entry };
-  res.render('Layout', initState);
+router.route('/:id/edit')
+  .get(async (req, res) => {
+    const entry = await Entry.findOne({ where: { id: req.params.id } });
+    const initState = { entry };
+    res.render('Layout', initState);
+  })
+  .post(async (req, res) => {
+    const { id } = req.params;
+    await Entry.update(req.body, { where: { id } });
+    res.redirect(`/entries/${id}`);
+  });
+
+router.get('/:id/delete', async (req, res) => {
+  const { id } = req.params;
+  await Entry.destroy({ where: { id } });
+  res.redirect('/');
 });
 
 router.get('/:id', async (req, res) => {
